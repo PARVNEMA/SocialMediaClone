64ff1387405049370f1e226a7cd99389793b9bcf
PARVNEMA <115030244+PARVNEMA@users.noreply.github.com>
2023-11-23 12:36:37 +0530

    Update script.js

--- script.js
+++ script.js
@@ -3,3 +3,16 @@
  function toggleMenu(){
     profileMenu.classList.toggle("open-menu");
  }
+ let sidebaractivity=document.getElementById("sidebar-activity");
+ let morelink=document.getElementById("showMorelink");
+
+ function toggleactivity(){
+   sidebaractivity.classList.toggle("open-activity");
+   if(sidebaractivity.classList.contains("open-activity")){
+      morelink.innerHTML="Show less <b>-</b>"
+   }
+   else{
+      morelink.innerHTML="Show more <b>+</b>"
+
+   }
+ }
